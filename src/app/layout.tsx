3f84d96fdf0e0f88807727b0dbd022feb23c3f43
Junyoung <72692386+junzerokim@users.noreply.github.com>
2024-04-26 17:55:02 +0900

    :dizzy: Add: Project Init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
-import './styles/global.css';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Next.js',
@@ -12,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
