8456875756a4580bc55da31b04031851a4210ffd
Junyoung <72692386+junzerokim@users.noreply.github.com>
2024-05-01 23:13:22 +0900

    :dizzy: Add: Login Page & SignUp Page

--- src/app/pages/auth/signIn/page.tsx
+++ src/app/pages/auth/signIn/page.tsx
@@ -1,14 +1,26 @@
 import Link from 'next/link';
+import { SiNaver } from 'react-icons/si';
 
 export default function SignIn() {
   return (
-    <>
-      <div>VS.GG 로고</div>
-      <button>네이버로 3초만에 시작하기</button>
-      <div>
-        <div>아직 VS.GG 멤버가 아니신가요?</div>
-        <Link href="/pages/auth/signUp">회원가입</Link>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <div className="mb-10 mt-auto font-['SBAggroB'] text-5xl text-[#8A1F21] md:text-8xl">
+        VS.GG
       </div>
-    </>
+      <div className="mb-3 flex items-center justify-center gap-2 rounded-3xl bg-black p-2 px-10">
+        <SiNaver color="white" />
+        <button className="text-white">네이버로 3초만에 시작하기</button>
+      </div>
+      <div className="flex gap-2">
+        <div>아직 VS.GG 멤버가 아니신가요?</div>
+        <Link href="/pages/auth/signUp" className="text-[#8A1F21]">
+          회원가입
+        </Link>
+      </div>
+      <div className="mb-20 mt-auto flex gap-5">
+        <div>이용약관</div>
+        <div className="text-gray-400">개인정보처리방침</div>
+      </div>
+    </div>
   );
 }
--- src/app/pages/auth/signUp/page.tsx
+++ src/app/pages/auth/signUp/page.tsx
@@ -2,13 +2,17 @@ import Link from 'next/link';
 
 export default function SignUp() {
   return (
-    <>
-      <div>VS.GG 로고</div>
-      <button>네이버로 가입하기</button>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <div className="font-['SBAggroB'] text-4xl text-[#8A1F21]">VS.GG</div>
+      <div className="flex w-full items-center justify-center gap-20 px-40">
+        <div className="h-1 w-full bg-gray-800"></div>
+        <div className="w-30">간단 회원가입</div>
+        <div className="h-1 w-full bg-gray-800"></div>
+      </div>
       <div>
         <div>이미 가입하셨다면?</div>
         <Link href="/pages/auth/signIn">바로 로그인하기</Link>
       </div>
-    </>
+    </div>
   );
 }
