114f0a097356e152d68976ae42d18e26c9c4b483
MH <dico659@gmail.com>
2024-05-13 03:22:58 +0900

    🔒️ Fix: hide exposed url

--- src/app/(routes)/api/oauth/naver/login/route.ts
+++ src/app/(routes)/api/oauth/naver/login/route.ts
@@ -1,10 +1,11 @@
 // /api/oauth/naver/login/route.ts
 import { redirect } from 'next/navigation';
 import { NextRequest, NextResponse } from 'next/server';
+import { NEXT_AUTH_API_URL } from '@/app/constants';
 
 export async function GET(req: NextRequest) {
   const redirectUri = 'http://localhost:3000/api/auth/callback/naver';
-  const clientUrl = `http://ec2-13-236-153-246.ap-southeast-2.compute.amazonaws.com/oauth2/authorization/naver?redirect_uri=${encodeURIComponent(redirectUri)}&mode=login`;
+  const clientUrl = `${NEXT_AUTH_API_URL}/oauth2/authorization/naver?redirect_uri=${encodeURIComponent(redirectUri)}&mode=login`;
 
   // return NextResponse.redirect(clientUrl);
   redirect(clientUrl);
