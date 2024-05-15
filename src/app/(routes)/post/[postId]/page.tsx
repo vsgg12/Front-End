9a295c4688ebfd3a1d38441a71e10ac66f845074
MH <dico659@gmail.com>
2024-05-15 15:59:34 +0900

    ✨ Add: post read voting start

--- src/app/(routes)/post/[postId]/page.tsx
+++ src/app/(routes)/post/[postId]/page.tsx
@@ -17,16 +17,36 @@ export default function PostRead({
 }: {
   params: IPostReadParams;
 }): JSX.Element {
+  const ingameInfos = [
+    { id: 0, champion: '챔1', position: 'top', tier: '그마' },
+    { id: 1, champion: '챔1', position: 'top', tier: '그마' },
+    { id: 2, champion: '챔1', position: 'top', tier: '그마' },
+    { id: 3, champion: '챔1', position: 'top', tier: '그마' },
+    { id: 4, champion: '챔1', position: 'top', tier: '그마' },
+  ];
+
+  const usePost = {
+    memberId: 0,
+    postId: params.postId,
+    title: '제목입니다',
+    content: `
+    Lorem Ipsum is simply dummy text of the printing and typesetting
+    industry. Lorem Ipsum has been the industry's standard dummy
+    text ever since the 1500s, when an unknown printer took a galley
+    of type and scrambled it to make a type specimen book. It has
+    survived not only five centuries, but also the leap into
+    electronic typesetting, remaining essentially unchanged. It was
+    popularised in the 1960s with the release of Letraset sheets
+    containing Lorem Ipsum passages, and more recently with desktop
+    publishing software like Aldus PageMaker including versions of
+    Lorem Ipsum.
+    `,
+  };
+
   useEffect(() => {
     console.log('post read page 렌더');
     //voting한 postId === postId면 해제하는 코드
   }, []);
-
-  // const changeHashtagStyle = (index: number) => {
-  //   const common =
-  //     'mb-1 mr-3 flex w-fit flex-row items-center justify-center rounded-[150px] border-2 px-[15px] py-[5px]';
-  //   return index === 0 ? common + ' border-[#000000]' : common;
-  // };
 
   return (
     <>
