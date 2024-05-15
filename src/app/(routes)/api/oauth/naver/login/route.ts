88c342c7fbdec79caf61d7d008b4657e86509faa
MH <dico659@gmail.com>
2024-05-16 02:09:19 +0900

    🚀 Fix: url

--- src/app/(routes)/api/oauth/naver/login/route.ts
+++ src/app/(routes)/api/oauth/naver/login/route.ts
@@ -4,7 +4,7 @@ import { NEXT_AUTH_API_URL } from '../../../../../constants';
 export async function GET(req: NextRequest) {
   const redirectUri = `http://localhost:3000/api/auth/callback`;
 
-  const clientUrl = `${NEXT_AUTH_API_URL}/oauth2/authorization/naver?redirect_uri=${encodeURIComponent(redirectUri)}&mode=login`;
+  const clientUrl = `${process.env.NEXT_AUTH_API_URL}/oauth2/authorization/naver?redirect_uri=${encodeURIComponent(redirectUri)}&mode=login`;
 
   // Redirect the user to Naver's OAuth2 authorization endpoint
   return NextResponse.redirect(clientUrl);
