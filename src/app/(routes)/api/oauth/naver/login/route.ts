468aef4874e564b823356aad5fad6ae3753e1a98
MH <dico659@gmail.com>
2024-05-16 02:15:34 +0900

    🚀 Fix: eslint on

--- src/app/(routes)/api/oauth/naver/login/route.ts
+++ src/app/(routes)/api/oauth/naver/login/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { NEXT_AUTH_API_URL } from '../../../../../constants';
+import { NEXT_AUTH_API_URL } from '@/app/constants';
 
 export async function GET(req: NextRequest) {
   const redirectUri = `http://localhost:3000/api/auth/callback`;
 
-  const clientUrl = `${process.env.NEXT_AUTH_API_URL}/oauth2/authorization/naver?redirect_uri=${encodeURIComponent(redirectUri)}&mode=login`;
+  const clientUrl = `${NEXT_AUTH_API_URL}/oauth2/authorization/naver?redirect_uri=${encodeURIComponent(redirectUri)}&mode=login`;
 
   // Redirect the user to Naver's OAuth2 authorization endpoint
   return NextResponse.redirect(clientUrl);
