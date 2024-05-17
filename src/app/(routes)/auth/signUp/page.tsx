855f1753226215287d081ad3b24b1d53e01d3b62
MH <dico659@gmail.com>
2024-05-18 03:55:17 +0900

    🚀 Add: Suspense at signup page

--- src/app/(routes)/auth/signUp/page.tsx
+++ src/app/(routes)/auth/signUp/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { ICreateMemberProps } from '@/app/types/form';
 import { useSearchParams } from 'next/navigation';
@@ -91,179 +91,188 @@ export default function SignUp() {
   }, [name, email, profile_image, gender, mobile, age]);
 
   return (
-    <div className="mt-12 flex h-full flex-col items-center justify-center gap-10">
-      <Link href="/">
-        <div className="font-['SBAggroB'] text-4xl text-[#8A1F21]">VS.GG</div>
-      </Link>
-      <div className="flex w-full items-center justify-center px-40">
-        <div className="flex w-1/2 items-center">
-          <div className="h-0.5 grow bg-[#D9D9D9]"></div>
-          <div className="mx-[30px] text-[#7B7B7B]">간단 회원가입</div>
-          <div className="h-0.5 grow bg-[#D9D9D9]"></div>
+    <Suspense>
+      <div className="mt-12 flex h-full flex-col items-center justify-center gap-10">
+        <Link href="/">
+          <div className="font-['SBAggroB'] text-4xl text-[#8A1F21]">VS.GG</div>
+        </Link>
+        <div className="flex w-full items-center justify-center px-40">
+          <div className="flex w-1/2 items-center">
+            <div className="h-0.5 grow bg-[#D9D9D9]"></div>
+            <div className="mx-[30px] text-[#7B7B7B]">간단 회원가입</div>
+            <div className="h-0.5 grow bg-[#D9D9D9]"></div>
+          </div>
         </div>
-      </div>
 
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-10">
-        <div className="flex flex-col gap-2">
-          <p>이메일</p>
-          <input
-            type="text"
-            readOnly
-            value={naverValue.email}
-            className="su-i-blocked"
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <p>
-            전화번호
-            {/* <span className="text-red-500">*</span> */}
-          </p>
-          <div className="flex gap-2">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-10"
+        >
+          <div className="flex flex-col gap-2">
+            <p>이메일</p>
             <input
-              value={naverValue.mobile}
               type="text"
               readOnly
-              className="su-i-blocked mb-1 grow"
+              value={naverValue.email}
+              className="su-i-blocked"
             />
-            {/* <button className="su-btn">인증요청</button> */}
           </div>
-          {/* <div className="flex gap-2">
+          <div className="flex flex-col gap-2">
+            <p>
+              전화번호
+              {/* <span className="text-red-500">*</span> */}
+            </p>
+            <div className="flex gap-2">
+              <input
+                value={naverValue.mobile}
+                type="text"
+                readOnly
+                className="su-i-blocked mb-1 grow"
+              />
+              {/* <button className="su-btn">인증요청</button> */}
+            </div>
+            {/* <div className="flex gap-2">
             <input type="text" required className="su-i" />
             <button type="button" className="su-btn bg-[#8A1F21]  text-white">
               인증완료
             </button>
           </div> */}
-          {/* {wrongNumber && (
+            {/* {wrongNumber && (
             <span className="pl-5 text-xs text-[#8A1F21]">
               잘못된 인증번호입니다.
             </span>
           )} */}
-        </div>
-        <div className="flex flex-col gap-2">
-          <p>닉네임</p>
-          <div className="flex gap-2">
-            <div className="flex grow flex-col">
-              <input
-                type="text"
-                {...register('nickname', {
-                  required: '닉네임은 필수 항목입니다.',
-                  minLength: {
-                    value: 2,
-                    message: '닉네임은 2글자 이상이어야 합니다.',
-                  },
-                  maxLength: {
-                    value: 20,
-                    message: '닉네임은 20글자 이하이어야 합니다.',
-                  },
-                })}
-                className="su-i"
-              />
-            </div>
-
-            <button
-              type="button"
-              onClick={handleCheckNickname}
-              className="su-btn text-[#8A1F21] "
-            >
-              중복확인
-            </button>
           </div>
-          {errors.nickname && (
-            <span className="pl-5 text-xs text-[#8A1F21]">
-              {errors.nickname.message}
-            </span>
-          )}
+          <div className="flex flex-col gap-2">
+            <p>닉네임</p>
+            <div className="flex gap-2">
+              <div className="flex grow flex-col">
+                <input
+                  type="text"
+                  {...register('nickname', {
+                    required: '닉네임은 필수 항목입니다.',
+                    minLength: {
+                      value: 2,
+                      message: '닉네임은 2글자 이상이어야 합니다.',
+                    },
+                    maxLength: {
+                      value: 20,
+                      message: '닉네임은 20글자 이하이어야 합니다.',
+                    },
+                  })}
+                  className="su-i"
+                />
+              </div>
 
-          {isNicknameCheck && sameNickname && (
-            <span className="pl-5 text-xs text-[#8A1F21]">
-              중복된 닉네임입니다.
-            </span>
-          )}
-          {isNicknameCheck && !sameNickname && (
-            <span className="pl-5 text-xs text-[#7f9cdb]">
-              사용 가능한 닉네임입니다.
-            </span>
-          )}
-        </div>
+              <button
+                type="button"
+                onClick={handleCheckNickname}
+                className="su-btn text-[#8A1F21] "
+              >
+                중복확인
+              </button>
+            </div>
+            {errors.nickname && (
+              <span className="pl-5 text-xs text-[#8A1F21]">
+                {errors.nickname.message}
+              </span>
+            )}
 
-        <div className="h-0.5 bg-[#D9D9D9]"></div>
-        <div className="flex flex-col gap-10">
-          <div className="flex items-center gap-5">
-            <input
-              type="checkbox"
-              className="size-6 accent-[#8A1F21]"
-              name="agreeAge"
-              checked={checkboxes.agreeAge}
-              onChange={handleCheckboxChange}
-              required
-            />
-            <div className="flex flex-col gap-1">
-              <p>만 14세 이상입니다. (필수)</p>
-              <p className="text-xs text-[#8A1F21]">
-                만 14세 이상만 가입이 가능합니다.
+            {isNicknameCheck && sameNickname && (
+              <span className="pl-5 text-xs text-[#8A1F21]">
+                중복된 닉네임입니다.
+              </span>
+            )}
+            {isNicknameCheck && !sameNickname && (
+              <span className="pl-5 text-xs text-[#7f9cdb]">
+                사용 가능한 닉네임입니다.
+              </span>
+            )}
+          </div>
+
+          <div className="h-0.5 bg-[#D9D9D9]"></div>
+          <div className="flex flex-col gap-10">
+            <div className="flex items-center gap-5">
+              <input
+                type="checkbox"
+                className="size-6 accent-[#8A1F21]"
+                name="agreeAge"
+                checked={checkboxes.agreeAge}
+                onChange={handleCheckboxChange}
+                required
+              />
+              <div className="flex flex-col gap-1">
+                <p>만 14세 이상입니다. (필수)</p>
+                <p className="text-xs text-[#8A1F21]">
+                  만 14세 이상만 가입이 가능합니다.
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center gap-5">
+              <input
+                type="checkbox"
+                className="size-6 accent-[#8A1F21]"
+                name="agreeTerms"
+                checked={checkboxes.agreeTerms}
+                onChange={handleCheckboxChange}
+                required
+              />
+              <p>
+                <span className="font-bold text-[#8A1F21]">이용약관</span>에
+                동의합니다. (필수)
               </p>
             </div>
+            <div className="flex items-center gap-5">
+              <input
+                type="checkbox"
+                className="size-6 accent-[#8A1F21]"
+                name="agreePrivacy"
+                checked={checkboxes.agreePrivacy}
+                onChange={handleCheckboxChange}
+                required
+              />
+              <p>
+                <span className="font-bold text-[#8A1F21]">
+                  개인정보처리방침
+                </span>
+                에 동의합니다. (필수)
+              </p>
+            </div>
+            <div className="flex items-center gap-5">
+              <input
+                type="checkbox"
+                className="size-6 accent-[#8A1F21]"
+                name="agreePromotion"
+                checked={checkboxes.agreePromotion}
+                onChange={handleCheckboxChange}
+              />
+              <p>
+                서비스 홍보 및 마케팅 목적의{' '}
+                <span className="font-bold text-[#8A1F21]">
+                  개인정보처리방침
+                </span>
+                에 동의합니다. (선택)
+              </p>
+            </div>
+            <div className="h-0.5 w-full bg-[#D9D9D9]"></div>
+            <div className="flex items-center gap-5">
+              <input
+                type="checkbox"
+                className="size-6 accent-[#8A1F21]"
+                checked={Object.values(checkboxes).every(Boolean)}
+                onChange={(e) => handleCheckAll(e.target.checked)}
+              />
+              <p>모두 동의합니다.</p>
+            </div>
           </div>
-          <div className="flex items-center gap-5">
-            <input
-              type="checkbox"
-              className="size-6 accent-[#8A1F21]"
-              name="agreeTerms"
-              checked={checkboxes.agreeTerms}
-              onChange={handleCheckboxChange}
-              required
-            />
-            <p>
-              <span className="font-bold text-[#8A1F21]">이용약관</span>에
-              동의합니다. (필수)
-            </p>
-          </div>
-          <div className="flex items-center gap-5">
-            <input
-              type="checkbox"
-              className="size-6 accent-[#8A1F21]"
-              name="agreePrivacy"
-              checked={checkboxes.agreePrivacy}
-              onChange={handleCheckboxChange}
-              required
-            />
-            <p>
-              <span className="font-bold text-[#8A1F21]">개인정보처리방침</span>
-              에 동의합니다. (필수)
-            </p>
-          </div>
-          <div className="flex items-center gap-5">
-            <input
-              type="checkbox"
-              className="size-6 accent-[#8A1F21]"
-              name="agreePromotion"
-              checked={checkboxes.agreePromotion}
-              onChange={handleCheckboxChange}
-            />
-            <p>
-              서비스 홍보 및 마케팅 목적의{' '}
-              <span className="font-bold text-[#8A1F21]">개인정보처리방침</span>
-              에 동의합니다. (선택)
-            </p>
-          </div>
-          <div className="h-0.5 w-full bg-[#D9D9D9]"></div>
-          <div className="flex items-center gap-5">
-            <input
-              type="checkbox"
-              className="size-6 accent-[#8A1F21]"
-              checked={Object.values(checkboxes).every(Boolean)}
-              onChange={(e) => handleCheckAll(e.target.checked)}
-            />
-            <p>모두 동의합니다.</p>
-          </div>
-        </div>
-        <button
-          type="submit"
-          className="rounded-full bg-[#8A1F21] py-2 text-white"
-        >
-          가입하기
-        </button>
-      </form>
-    </div>
+          <button
+            type="submit"
+            className="rounded-full bg-[#8A1F21] py-2 text-white"
+          >
+            가입하기
+          </button>
+        </form>
+      </div>
+    </Suspense>
   );
 }
