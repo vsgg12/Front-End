08ea9d36210ee3cafc0a872fdc00cf91d4cbb35a
MH <dico659@gmail.com>
2024-05-18 16:06:37 +0900

    ✨ Fix: next.cofig

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_AUTH_API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
